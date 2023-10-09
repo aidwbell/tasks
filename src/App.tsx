35e0ca16d81f100d247f5bf76a7c4ee71236d895
Aidan <aidwbell@udel.edu>
2023-10-08 21:40:10 -0400

    Added comments

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Aidan Bell The all
-                new 2023 992 911 GT3RS
+                UD CISC275 with React Hooks and TypeScript Aidan Bell
             </header>
             HEAD
             <div>
@@ -27,6 +26,7 @@ function App(): JSX.Element {
                 </p>
             </div>
             <div>
+                <h2>The all new 2023 992 911 GT3RS</h2>
                 <img
                     src="https://files.porsche.com/filestore/image/multimedia/none/992-gt3-rs-modelimage-sideshot/model/cfbb8ed3-1a15-11ed-80f5-005056bbdc38/porsche-model.png"
                     height="540px"
